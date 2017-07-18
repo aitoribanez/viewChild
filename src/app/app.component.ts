62a6b878d1aefe6d0bda14c4bd87e4262c31951b
Aitor Ibañez <aitiba@gmail.com>
2017-07-18 21:16:19 +0200

    - More changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,8 @@ import { Component } from '@angular/core';
 			   <app-number-parent></app-number-parent>
 			   <app-stopwatch-parent></app-stopwatch-parent>
 			   <app-cpcolor-parent></app-cpcolor-parent>
-			   <app-cptheme>
-			   	<app-ui-element></app-ui-element>
-			   </app-cptheme>
+			   <app-cptheme></app-cptheme>
+			   <app-ui-element></app-ui-element>
              `
 })
 export class AppComponent { }
--- src/app/cptheme.component.ts
+++ src/app/cptheme.component.ts
@@ -5,20 +5,20 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
   templateUrl: './cptheme.component.html'
 })
 export class CpThemeComponent implements AfterViewInit {
-/*   @ViewChild('name') private elName: ElementRef;
-  @ViewChild('city') private elCity: ElementRef; */
+  @ViewChild('name') private elName: ElementRef;
+  @ViewChild('city') private elCity: ElementRef;
 
   ngAfterViewInit() {
     console.log('theme init')
-   /*  this.elName.nativeElement.style.backgroundColor = 'cyan';
-    this.elName.nativeElement.style.color = 'red'; */
+    this.elName.nativeElement.style.backgroundColor = 'cyan';
+    this.elName.nativeElement.style.color = 'red';
     /* this.elCity.nativeElement.style.backgroundColor = 'cyan';
     this.elCity.nativeElement.style.color = 'red'; */
   }
 
   changeColor(backgroundColor, color) {
-   /*  this.elName.nativeElement.style.backgroundColor = backgroundColor;
+    this.elName.nativeElement.style.backgroundColor = backgroundColor;
     this.elName.nativeElement.style.color = color;
-    */ console.log('change color to', color)
+    console.log('change color to', color)
   }
 }
